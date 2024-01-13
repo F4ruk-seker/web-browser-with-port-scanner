808d2e5a4026f127f7370123e22cff3dac6ae4d4
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-13 21:41:59 +0300

    create panel

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+import os
+
+
+mongo_client: MongoClient = MongoClient(os.getenv('MONGO_DB_URL'))
+mydb = mongo_client[os.getenv('MONGO_DB_NAME')]
+mycol = mydb["customers"]
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return [{'id': str(x.get('_id')), 'ip_data': x.get('ip_data')} for x in mycol.find({})]
+
+
+@app.get("/get/{_id}/")
+async def get_data(_id: str):
+    obj = mycol.find_one({'_id': ObjectId(_id)})
+    obj['_id'] = str(obj['_id'])
+    return obj
